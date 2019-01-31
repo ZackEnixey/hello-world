f1320263a4e66d0c6264ec1c5b5f2c28f15ffc84
ZackEnixey <46425931+ZackEnixey@users.noreply.github.com>
2019-01-31 10:00:59 +0100

    adding text from my Gmail (#4)
    alskdfj lkfj lajfalksj lk jf

---
+++ justFOrAnother.js
@@ -0,0 +1,29 @@
+Hi Zoran,
+
+I noticed that it's been about three weeks since you signed up with Logojoy, but that you still hadn't downloaded your favorite logo.
+
+As such, I thought our prices may have been too expensive for your budget?
+
+If so, I'd like to offer you a 50% discount on our premium package.
+
+To claim your offer, simply:
+
+---
+
+1. Login: to your account by clicking this link.
+
+2. Choose your favorite logo: by clicking on it and pressing "buy".
+
+3. Discount applied: you'll see your 50% discount on the checkout page.
+
+---
+
+I hope this helps?
+
+Best,
+
+
+Felicia Cusi
+Head of Customer Success
+
+P.S. Just reply to this email if you have any questions!
